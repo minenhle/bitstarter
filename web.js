205ca7be942391303e84ba6d7c52f1bb55518f3f
minenhle <minenhle08@gmail.com>
2013-07-06 18:03:46 +0000

    Correction to script

--- web.js
+++ web.js
@@ -4,7 +4,8 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   response.send('Hello World 2!');
-  response.send(fs.readFileSync(index.html));
+  response.send(fs.readFileSync(index.html))
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
